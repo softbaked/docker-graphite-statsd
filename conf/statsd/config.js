ecbffc5921880e331e780370bd3c0de5fa7c9d3c
Prathan Thananart <pop@page365.net>
2015-12-12 23:29:56 +0700

    Update config for our use case

--- conf/statsd/config.js
+++ conf/statsd/config.js
@@ -1,6 +1,16 @@
 {
-  "graphiteHost": "127.0.0.1",
-  "graphitePort": 2003,
-  "port": 8125,
-  "flushInterval": 10000
+  graphiteHost: "127.0.0.1",
+  graphitePort: 2003,
+  port: 8125,
+  flushInterval: 60000,
+  deleteIdleStats: true,
+  deleteGauges: false,
+  graphite: {
+    legacyNamespace: false,
+    globalPrefix:  "",
+    prefixCounter: "",
+    prefixTimer:   "",
+    prefixGauge:   "",
+    prefixSet:     ""
+  }
 }
